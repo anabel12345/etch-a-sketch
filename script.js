5ba2ba23106d9718ab31bc132527303460be08c6
Annabelle <lilacpaw2018@gmail.com>
2023-07-25 11:36:30 -0900

    Set up the square in the middle+background colors

---
+++ script.js
@@ -0,0 +1,15 @@
+const container=document.querySelector('.container');
+for(let i=0;i<16;i++){
+    let x = document.createElement('div')
+    x.style.backgroundColor='white';
+    x.style.height='60px'
+    x.style.width='60px'
+
+   
+    container.appendChild(x)
+    console.log(document.querySelector('div'))
+}
+
+
+
+
