dc4d215af9ccf149ec089d3be1859b8a91e62e18
Annabelle <lilacpaw2018@gmail.com>
2023-07-25 11:42:28 -0900

    make the etch-a-sketch functional

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 const container=document.querySelector('.container');
 for(let i=0;i<16;i++){
     let x = document.createElement('div')
@@ -5,6 +11,10 @@ for(let i=0;i<16;i++){
     x.style.height='60px'
     x.style.width='60px'
 
+    x.onmouseenter=function(){
+        console.log('happy')
+        x.style.backgroundColor='black';
+    }
    
     container.appendChild(x)
     console.log(document.querySelector('div'))
