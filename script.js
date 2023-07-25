f4b769bc219f9cfcca88703b62f6de44a1b2128f
Annabelle <lilacpaw2018@gmail.com>
2023-07-25 14:06:34 -0900

    added styles

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+let size=prompt("How complex should the grid be?")
+if(size<=0||size>100){
+    size=prompt('type a number between 1 and 100!!')
+}
 
 
 
 
-
-
+let dimen=860/size;
 const container=document.querySelector('.container');
-for(let i=0;i<16;i++){
+for(let i=0;i<size*size;i++){
     let x = document.createElement('div')
-    x.style.backgroundColor='white';
-    x.style.height='60px'
-    x.style.width='60px'
+    x.style.backgroundColor='white'
+  console.log(dimen)
+    x.style.height=dimen+'px'
+    x.style.width=dimen+'px'
 
     x.onmouseenter=function(){
-        console.log('happy')
+        
         x.style.backgroundColor='black';
+  
     }
    
     container.appendChild(x)
-    console.log(document.querySelector('div'))
+    
 }
 
 
